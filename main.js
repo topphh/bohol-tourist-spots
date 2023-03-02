2b7d614ccf19c71c933e2eb26797924176c40f8b
cristophergit <email.cristopher@gmail.com>
2023-03-02 23:15:16 +0800

    add spots-info.json, javascript function to loop through all elements in spots-info.json, remove unnecesary lines, add commentary.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var map = L.map(
+// Create a map and set is center to the center of bohol.
+let map = L.map(
     "map",
     {
         center: [9.8347, 124.2829],
@@ -9,19 +10,26 @@ var map = L.map(
     }
 );
 
+// create a new tile layer using the OpenStreetMap 
 var title_layer = L.tileLayer(
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     {"attribution": "Data by \u0026copy; \u003ca target=\"_blank\" href=\"http://openstreetmap.org\"\u003eOpenStreetMap\u003c/a\u003e, under \u003ca target=\"_blank\" href=\"http://www.openstreetmap.org/copyright\"\u003eODbL\u003c/a\u003e.", "detectRetina": false, "maxNativeZoom": 18, "maxZoom": 18, "minZoom": 0, "noWrap": false, "opacity": 1, "subdomains": "abc", "tms": false}
 ).addTo(map);
 
-var chocolatehills = L.marker([9.8174, 124.1435], {}).addTo(map);
-var popup1 = L.popup({"maxWidth": "100%"});
-var popup_html = $(`<div id="popup_html" style="width: 100.0%; height: 100.0%;"><h3>Chocolate Hills</h3><img src="res/images/chocolate-hills.jpg" width="240" height="180">
-                    <div>The hills are located throughout the towns of Carmen, 
-                    Batuan and Sagbayan and consist of about 1,776 mounds of the same general shape. 
-                    During the dry season when precipitation in inadequate,
-                    the grass-covered hills turn chocolate brown, 
-                    hence the name in reference to a branded confection.</div></div>`)[0];
-popup1.setContent(popup_html);
-chocolatehills.bindPopup(popup1);
-chocolatehills.bindTooltip(`<div>Chocolate Hills</div>`, {"sticky": true});
+
+// loop through all elements in spots-info.json then and place the information to a spots in the map.
+fetch("res/spots-info.json")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(item => {
+      console.log(`${item.name}, ${item.address}, ${item.description}`);
+      var popup = L.popup({"maxWidth": "100%"});
+      let spotmark = L.marker([item.latitude, item.longitude], {}).addTo(map);
+      var popup_html = $(`<div id="popup_html" style="width: 100.0%; height: 100.0%;"><h3>${item.name}</h3><img src="${item.imageSource}" width="240" height="180">
+                    <div>${item.description}</div></div>`)[0];
+        popup.setContent(popup_html);
+        spotmark.bindPopup(popup);
+        spotmark.bindTooltip(`<div>${item.name}</div>`, {"sticky": true});
+    });
+  })
+  .catch(error => console.error(error));
