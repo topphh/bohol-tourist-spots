0d6071c38243a23902df57dfd5dde9b7bec0dc25
cristophergit <email.cristopher@gmail.com>
2023-03-05 10:43:17 +0800

    add map max zoom

--- script/map.js
+++ script/map.js
@@ -13,7 +13,15 @@ let map = L.map(
 // create a new tile layer using the OpenStreetMap 
 var title_layer = L.tileLayer(
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {"attribution": "Data by \u0026copy; \u003ca target=\"_blank\" href=\"http://openstreetmap.org\"\u003eOpenStreetMap\u003c/a\u003e, under \u003ca target=\"_blank\" href=\"http://www.openstreetmap.org/copyright\"\u003eODbL\u003c/a\u003e.", "detectRetina": false, "maxNativeZoom": 18, "maxZoom": 18, "minZoom": 0, "noWrap": false, "opacity": 1, "subdomains": "abc", "tms": false}
+    {"attribution": "Data by \u0026copy; \u003ca target=\"_blank\" href=\"http://openstreetmap.org\"\u003eOpenStreetMap\u003c/a\u003e, under \u003ca target=\"_blank\" href=\"http://www.openstreetmap.org/copyright\"\u003eODbL\u003c/a\u003e.", 
+    "detectRetina": false, 
+    "maxNativeZoom": 18, 
+    "maxZoom": 18, 
+    "minZoom": 9, 
+    "noWrap": false, 
+    "opacity": 10, 
+    "subdomains": "abc",
+    "tms": false}
 ).addTo(map);
 
 
