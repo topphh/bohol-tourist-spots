b62fcc4c231e678ccfefeacf0e614fa2fc23554f
cristophergit <email.cristopher@gmail.com>
2023-03-05 18:44:21 +0800

    put style to map pop up

--- script/map.js
+++ script/map.js
@@ -17,25 +17,15 @@ fetch("res/spots-info.json")
   .then(response => response.json())
   .then(data => {
       data.forEach(item => {
-          const imagesHTML = item.imageSource.map(src => `<div class="spot-image"><img src="${src}" width="240" height="180"></div>`).join('');
+          const imagesHTML = item.imageSource.map(src => `<img  src="${src}">`).join('');
           const popup_html = $(`<div id="spot-popup">
-          <div id="spot-images">${imagesHTML}${item.imageSource.length > 1 ? '<div class="next-image-icon"><i class="fas fa-chevron-right"></i></div>' : ''}</div>
+          <div id="spot-images">${imagesHTML}</div>
           <div id="spot-name">${item.name}</div>
           <div id="spot-address">${item.address}</div>
           <div id="spot-description">${item.description}</div>
           </div>`)[0];
           const popup = L.popup({maxWidth: "100%"}).setContent(popup_html);
           const spotmark = L.marker([item.latitude, item.longitude], {}).addTo(map).bindPopup(popup).bindTooltip(`<div>${item.name}</div>`, {sticky: true});
-          if (item.imageSource.length > 1) {
-              let imageIndex = 0;
-              const spotImages = popup_html.querySelectorAll(".spot-image");
-              popup_html.querySelector(".next-image-icon").addEventListener("click", () => {
-                  imageIndex = (imageIndex + 1) % item.imageSource.length;
-                  spotImages.forEach(img => img.style.display = "none");
-                  spotImages[imageIndex].style.display = "block";
-              });
-          }
           console.log(`${item.name}, ${item.address}, ${item.description}`);
       });
-  })
-  .catch(error => console.error(error));
+  }).catch(error => console.error(error));
