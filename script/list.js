a5c592fae5e24d3a3ae9cf468561f8ad21265a25
cristophergit <email.cristopher@gmail.com>
2023-03-20 15:53:13 +0800

    add search on list view

--- script/list.js
+++ script/list.js
@@ -1,9 +1,26 @@
+function addTable(hotWord){
 $(document).ready(function() {
 	$.getJSON("./res/spots-info.json", function(data) {
 	  $.each(data, function(index, site) {
-		var row = "<tr><td>" + site.name + "</td><td>" + site.address + "</td><td>" + site.latitude + "</td><td>" + site.longitude + "</td><td>" + site.description + "</td></tr>";
-		$("#sites tbody").append(row);
+		const spotID = `${site.name} ${site.address} ${site.latitude} ${site.longitude}`.toLowerCase();
+		if (spotID.includes(hotWord) || hotWord === "all"){
+			var row = "<tr><td>" + site.name + "</td><td>" + site.address + "</td><td>" + site.latitude + "</td><td>" + site.longitude + "</td><td>" + site.description + "</td></tr>";
+			$("#sites tbody").append(row);
+		}
 	  });
 	});
   });
-  
+}
+function clearTable() {
+	$('#sites tbody').empty();
+}  
+addTable('all');
+
+//Search Implementation
+const inputField = document.querySelector('input');
+inputField.addEventListener('input', () => {
+  //clear table
+  	const searchStr = inputField.value.toLowerCase();
+	clearTable();
+	addTable(searchStr);
+});
